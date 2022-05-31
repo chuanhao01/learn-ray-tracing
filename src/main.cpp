2030577ddec9c964d34686f160430657a82aa085
chuanhao01 <34702921+chuanhao01@users.noreply.github.com>
2022-05-31 23:21:56 +0800

    [feat] changed up rendering square

--- src/main.cpp
+++ src/main.cpp
@@ -4,16 +4,25 @@ int main() {
   // Image
   const int IMAGE_WIDTH = 256;
   const int IMAGE_HEIGHT = 256;
+  const int COLOR_MAX = 255;
 
   // Render
   // Doing it from top left
-  std::cout << "P3\n" << IMAGE_WIDTH << ' ' << IMAGE_HEIGHT << "\n255\n";
+  std::cout << "P3"
+            << "\n";
+  std::cout << IMAGE_WIDTH << " " << IMAGE_HEIGHT << "\n";
+  std::cout << "255"
+            << "\n";
 
+  // (y, x), (j, i)
   for (int j = 0; j < IMAGE_HEIGHT; j++) {
+    // Per column
     for (int i = 0; i < IMAGE_WIDTH; i++) {
-      // (y, x), (j, i)
-      auto r = double(i) / (IMAGE_WIDTH - 1);
-      auto g = double(j) / (IMAGE_HEIGHT - 1);
+      // Per row
+      // Color mapping to 0.0 - 1.0 from 0 - 255
+      // Need to have length wrap around 0 - 255
+      auto r = double(i % COLOR_MAX) / COLOR_MAX;
+      auto g = double(j % COLOR_MAX) / COLOR_MAX;
       auto b = 0.25;
 
       int ir = static_cast<int>(255.999 * r);
