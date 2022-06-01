f728204df9a30b95f6c6ff39f756c354a9bda7bf
chuanhao01 <34702921+chuanhao01@users.noreply.github.com>
2022-06-01 14:31:26 +0800

    [feat] more comments and stderr code

--- src/main.cpp
+++ src/main.cpp
@@ -17,14 +17,19 @@ int main() {
   // (y, x), (j, i)
   for (int j = 0; j < IMAGE_HEIGHT; j++) {
     // Per column
+    std::cerr << "\r"
+              << "Scanlines remaining: " << IMAGE_HEIGHT - j - 1 << std::flush;
     for (int i = 0; i < IMAGE_WIDTH; i++) {
       // Per row
       // Color mapping to 0.0 - 1.0 from 0 - 255
       // Need to have length wrap around 0 - 255
-      auto r = double(i % COLOR_MAX) / COLOR_MAX;
-      auto g = double(j % COLOR_MAX) / COLOR_MAX;
+      auto r = double(i % (COLOR_MAX + 1)) / COLOR_MAX;
+      auto g = double(j % (COLOR_MAX + 1)) / COLOR_MAX;
       auto b = 0.25;
 
+      // Math is because truncation
+      // So 0 - 255 = 0 - 254.9999 = 0 - 254
+      // Hence needs 0 - 256 = 0 - 255.999 = 0 - 255
       int ir = static_cast<int>(255.999 * r);
       int ig = static_cast<int>(255.999 * g);
       int ib = static_cast<int>(255.999 * b);
@@ -32,4 +37,7 @@ int main() {
       std::cout << ir << " " << ig << " " << ib << "\n";
     }
   }
+  std::cerr << "\n"
+            << "Done!"
+            << "\n";
 }
