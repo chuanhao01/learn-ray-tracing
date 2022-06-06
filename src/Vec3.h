06ec5789f7f385efcfaabf44307db87505f03f25
chuanhao01 <34702921+chuanhao01@users.noreply.github.com>
2022-06-06 21:57:42 +0800

    [feat] added vec declaration

--- src/Vec3.h
+++ src/Vec3.h
@@ -4,6 +4,7 @@
 #include <cmath>
 #include <iostream>
 
+namespace vec {
 class Vec3 {
 public:
   Vec3() : e{0, 0, 0} {}
@@ -21,11 +22,35 @@ public:
   double operator[](int i) const {};
   double &operator[](int i){};
 
+  // Return a reference since this changes the left hand side to be the same
   Vec3 &operator+=(const Vec3 &v){};
   Vec3 &operator-=(const Vec3 &v){};
+  Vec3 &operator*=(double t){};
+  Vec3 &operator/=(double t){};
+
+  double length() const {};
 
 private:
   double e[3];
 };
+
+std::ostream &operator<<(std::ostream &cout, const Vec3 &v){};
+
+Vec3 operator+(const Vec3 &v1, const Vec3 &v2){};
+Vec3 operator-(const Vec3 &v1, const Vec3 &v2){};
+Vec3 operator*(const Vec3 &v, double t){};
+Vec3 operator*(double t, const Vec3 &v){};
+Vec3 operator/(Vec3 &v, double t){};
+Vec3 operator/(double t, Vec3 &v){};
+
+double dot(const Vec3 &v1, const Vec3 &v2){};
+Vec3 cross(const Vec3 &v1, const Vec3 &v2){};
+
+Vec3 unit_vector(const Vec3 &v){};
+
+using Point3 = Vec3; // 3D Point
+using Color = Vec3;  //
+
+} // namespace vec
 
 #endif
