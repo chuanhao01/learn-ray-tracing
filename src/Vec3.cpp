82ee2a7ba0f62afe8f49d6215f5b95a4699e901d
chuanhao01 <34702921+chuanhao01@users.noreply.github.com>
2023-10-12 10:32:17 +0800

    [feat] implement Vec3 class

---
+++ src/Vec3.cpp
@@ -0,0 +1,76 @@
+#include "Vec3.h"
+#include <iostream>
+
+namespace vec {
+Vec3 &Vec3::operator+=(const Vec3 &v) {
+  e[0] += v[0];
+  e[1] += v[1];
+  e[2] += v[2];
+  return *this;
+}
+Vec3 &Vec3::operator-=(const Vec3 &v) {
+  e[0] -= v[0];
+  e[1] -= v[1];
+  e[2] -= v[2];
+  return *this;
+}
+Vec3 &Vec3::operator*=(double t) {
+  e[0] *= t;
+  e[1] *= t;
+  e[2] *= t;
+  return *this;
+}
+Vec3 &Vec3::operator/=(double t) {
+  *this *= 1 / t;
+  return *this;
+}
+
+double Vec3::length() const { return sqrt(length_squared()); }
+double Vec3::length_squared() const {
+  return e[0] * e[0] + e[1] * e[1] + e[2] * e[2];
+}
+
+std::ostream &operator<<(std::ostream &cout, const Vec3 &v) {
+  return cout << "Vec3(" << v[0] << ", " << v[1] << ", " << v[2] << ")";
+}
+
+Vec3 operator+(const Vec3 &v1, const Vec3 &v2) {
+  Vec3 v = Vec3(v1);
+  v += v2;
+  return v;
+}
+Vec3 operator-(const Vec3 &v1, const Vec3 &v2) {
+  Vec3 v = Vec3(v1);
+  v -= v2;
+  return v;
+}
+Vec3 operator*(const Vec3 &v, double t) {
+  Vec3 nv = Vec3(v);
+  nv *= t;
+  return nv;
+}
+Vec3 operator*(double t, const Vec3 &v) {
+  Vec3 nv = Vec3(v);
+  nv *= t;
+  return nv;
+}
+Vec3 operator/(Vec3 &v, double t) {
+  Vec3 nv = Vec3(v);
+  nv /= t;
+  return nv;
+}
+
+// Since we are only dealing with 3D vecs, this is [3x1] * [1x3] always
+double dot(const Vec3 &v1, const Vec3 &v2) {
+  return v1[0] * v2[0] + v1[1] * v2[1] + v1[2] * v2[2];
+}
+Vec3 cross(const Vec3 &v1, const Vec3 &v2) {
+  return Vec3(v1[1] * v2[2] - v1[2] * v2[1], v1[2] * v2[0] - v1[0] * v2[2],
+              v1[0] * v2[1] - v1[1] * v2[0]);
+}
+
+Vec3 unit_vector(const Vec3 &v) {
+  Vec3 nv = Vec3(v);
+  return nv / nv.length();
+}
+} // namespace vec
--- src/Vec3.h
+++ src/Vec3.h
@@ -14,39 +14,38 @@ public:
   double y() { return e[1]; }
   double z() { return e[2]; }
 
-  // Return a new copy of the Vec3 for unary operator
-  // So it can be used for chaining/assignment
-  Vec3 operator-() const {};
+  // Overloading operators
+  // Subscript access to values
+  double operator[](int i) const { return e[i]; }
+  double &operator[](int i) { return e[i]; }
 
-  // Subscript
-  double operator[](int i) const {};
-  double &operator[](int i){};
+  Vec3 operator-() const { return Vec3(-e[0], -e[1], -e[2]); }
 
-  // Return a reference since this changes the left hand side to be the same
-  Vec3 &operator+=(const Vec3 &v){};
-  Vec3 &operator-=(const Vec3 &v){};
-  Vec3 &operator*=(double t){};
-  Vec3 &operator/=(double t){};
+  Vec3 &operator+=(const Vec3 &v);
+  Vec3 &operator-=(const Vec3 &v);
+  Vec3 &operator*=(double t);
+  Vec3 &operator/=(double t);
 
-  double length() const {};
+  double length() const;
+  double length_squared() const;
 
 private:
   double e[3];
 };
 
-std::ostream &operator<<(std::ostream &cout, const Vec3 &v){};
+std::ostream &operator<<(std::ostream &cout, const Vec3 &v);
 
-Vec3 operator+(const Vec3 &v1, const Vec3 &v2){};
-Vec3 operator-(const Vec3 &v1, const Vec3 &v2){};
-Vec3 operator*(const Vec3 &v, double t){};
-Vec3 operator*(double t, const Vec3 &v){};
-Vec3 operator/(Vec3 &v, double t){};
-Vec3 operator/(double t, Vec3 &v){};
+Vec3 operator+(const Vec3 &v1, const Vec3 &v2);
+Vec3 operator-(const Vec3 &v1, const Vec3 &v2);
+Vec3 operator*(const Vec3 &v, double t);
+Vec3 operator*(double t, const Vec3 &v);
+Vec3 operator/(Vec3 &v, double t);
+Vec3 operator/(double t, Vec3 &v);
 
-double dot(const Vec3 &v1, const Vec3 &v2){};
-Vec3 cross(const Vec3 &v1, const Vec3 &v2){};
+double dot(const Vec3 &v1, const Vec3 &v2);
+Vec3 cross(const Vec3 &v1, const Vec3 &v2);
 
-Vec3 unit_vector(const Vec3 &v){};
+Vec3 unit_vector(const Vec3 &v);
 
 using Point3 = Vec3; // 3D Point
 using Color = Vec3;  //
