1cbdd615e746b20a72cd682599dcd2cad3bab68c
chuanhao01 <34702921+chuanhao01@users.noreply.github.com>
2023-10-12 21:57:13 +0800

    [fix] color gradient based on y value

--- src/Ray.cpp
+++ src/Ray.cpp
@@ -5,8 +5,8 @@
 namespace ray {
 color::Color color_ray(const Ray &r) {
   auto unit_direction = vec::unit_vector(r.direction());
-  auto a = 0.5 * (unit_direction.length() + 1.0);
+  auto a = 0.5 * (unit_direction.y() + 1.0);
   return (1 - a) * color::Color(1.0, 1.0, 1.0) +
-         a * color::Color(0.5, 0.7, 1.0);
+         a * color::Color(0.0, 0.0, 1.0);
 }
 } // namespace ray
