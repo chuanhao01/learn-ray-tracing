dcf7b121821115db490b65c12c640891578dd742
chuanhao01 <34702921+chuanhao01@users.noreply.github.com>
2023-11-01 00:03:55 +0800

    [feat] add weekend-final-render.rs, fix cam
    Description
    Adding the weekend final render file
    Had a small bug where the defocus sample center did not account for the camera center

--- rust/src/objects.rs
+++ rust/src/objects.rs
@@ -9,6 +9,7 @@ use super::Materials;
 
 use super::Vec3;
 
+/// Simple Sphere object
 pub struct Sphere {
     pub center: Vec3,
     pub radius: f64,
@@ -60,5 +61,71 @@ impl Hittable for Hittables {
         match self {
             Hittables::Sphere(sphere) => sphere.hit(_ray, valid_t_interval),
         }
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use std::f64::INFINITY;
+
+    use super::*;
+
+    #[test]
+    fn test_sphere_hit() {
+        // Ensure the ray hits the sphere
+        let mat = Arc::new(Materials::None);
+        let s = Sphere {
+            center: Vec3::new_int(0, 0, 0),
+            radius: 1.0,
+            material: Arc::clone(&mat),
+        };
+        let r = Ray {
+            direction: Vec3::new_int(0, 0, 1),
+            origin: Vec3::new_int(0, 0, -2),
+        };
+        let hr = s
+            .hit(
+                &r,
+                Interval {
+                    l: 0.001,
+                    r: INFINITY,
+                },
+            )
+            .unwrap();
+        assert_eq!(hr.p, Vec3::new_int(0, 0, -1));
+        assert_eq!(hr.t, 1.0);
+        assert_eq!(hr.against_normal_unit, Vec3::new_int(0, 0, -1));
+        assert!(Arc::ptr_eq(&mat, &hr.material));
+        assert!(hr.front_face);
+
+        // Ensure you get the second t value
+        let hr = s
+            .hit(
+                &r,
+                Interval {
+                    l: 1.0,
+                    r: INFINITY,
+                },
+            )
+            .unwrap();
+        assert_eq!(hr.t, 3.0);
+
+        // Ensure interval out of range
+        assert!(s.hit(&r, Interval { l: 4.0, r: 5.0 },).is_none());
+
+        // Ensure that the ray does not hit the sphere
+        let r = Ray {
+            direction: Vec3::new_int(2, 0, 1),
+            ..r
+        };
+        assert!(s
+            .hit(
+                &r,
+                Interval {
+                    l: 0.001,
+                    r: INFINITY,
+                },
+            )
+            .is_none());
     }
 }
